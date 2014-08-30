e2bcfbeccb463234fc3f72d4a66448d775573784
Pablo Alejandro Costesich <rhfixer@gmail.com>
2014-08-30 14:28:22 -0300

    Add github endpoint

--- hookit.py
+++ hookit.py
@@ -9,3 +9,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Hello from Flask! (%s)' % datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+@app.route('/github')
+def github():
+    if request.method != 'POST':
+        return 405
+    with open('test', 'w') as f:
+        f.write(request.get_json())
+    return ':)'
