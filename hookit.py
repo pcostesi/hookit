e4fc0697697d2100bda2ce145e3889625d26b755
Pablo Alejandro Costesich <rhfixer@gmail.com>
2014-08-30 18:00:53 -0300

    Add logging

--- hookit.py
+++ hookit.py
@@ -99,5 +99,13 @@ def github():
     return 'Ok'
 
 
+
+import logging
+from logging.handlers import RotatingFileHandler
+file_handler = RotatingFileHandler('python.log', maxBytes=1024 * 1024 * 100, backupCount=20)
+file_handler.setLevel(logging.ERROR)
+app.logger.setLevel(logging.ERROR)
+app.logger.addHandler(file_handler)
+
 if __name__ == '__main__':
     app.run()
